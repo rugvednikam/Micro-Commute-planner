e5677145fa2607abcfc4940b30aec4d0337a1b30
rugvednikam <121861331+rugvednikam@users.noreply.github.com>
2025-08-11 16:29:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// IMPORTANT: This file contains your secret API keys.
+// Do NOT share it publicly or commit it to a public repository like GitHub.
+// Make sure to add "config.js" to your .gitignore file.
+
+const ORS_API_KEY = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImFjMzQwMTdiMzBjNDRjYTZhNjZhZmM0OWExYmEzNzdiIiwiaCI6Im11cm11cjY0In0=";
+
+const OWM_API_KEY = "dd9b8775f1e880440f6384f996c61199";
